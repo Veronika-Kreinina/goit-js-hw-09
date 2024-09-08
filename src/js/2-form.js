8321dd5003576a2d41217ed9b18b11ee9e0ce935
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-09-08 22:49:25 +0300

    add hw-9

---
+++ src/js/2-form.js
@@ -0,0 +1,10 @@
+const formData = { email: "", message: "" }
+
+const form = document.querySelector('.feedback-form')
+form.addEventListener('input', updateFormData())
+
+function updateFormData(event) {
+    
+}
+
+const feedback = 'feedback-form-state'
