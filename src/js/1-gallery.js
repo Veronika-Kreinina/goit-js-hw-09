b8922b0d55cbfa37066954382032eb41b5b5b3a8
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-09-08 23:13:26 +0300

    update vite.config

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -10,7 +10,7 @@ const images = [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
-  }
+  },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
