580408315f849c4a8149705440f9d5d024bd12ea
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-09-14 20:56:22 +0200

    update js-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,41 @@
 const formData = { email: "", message: "" }
 
 const form = document.querySelector('.feedback-form')
-form.addEventListener('input', updateFormData())
+const LS_key = "feedback-form-state"
+const emailInput = form.elements.email
+const messageTextarea =form.elements.message
 
-function updateFormData(event) {
-    
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lsData = localStorage.getItem(LS_key)
+
+    if (lsData) {
+    Object.assign(formData, JSON.parse(lsData));
+    emailInput.value = formData.email || "";
+    messageTextarea.value = formData.message || "";
 }
+});
 
-const feedback = 'feedback-form-state'
+form.addEventListener("input", event => {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+localStorage.setItem(LS_key, JSON.stringify(formData));
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    if (!formData.email || !formData.message) {
+        alert("Fill all fields");
+        return;
+    }
+
+    console.log(formData);
+   
+    localStorage.removeItem(LS_key)
+    formData.email = "";
+    formData.message = "";
+    form.reset()
+})
+
+
+
