c8012aa22b16b05f9b58df7153b10660eb5727be
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-09-15 17:27:09 +0200

    +

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const lightbox=new SimpleLightbox('.gallery a', { /* options */ });
 
 const images = [
   {
